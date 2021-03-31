a6cf625162aea49924a804fbb5c7cd24a225bd59
OthLah001 <oth.lahrimi1996@gmail.com>
2021-03-31 22:10:20 +0100

    Change the store depending on the menu configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { State, Store } from '@ngrx/store';
+import { TranslateService } from '@ngx-translate/core';
+import { IAppState, LanguageEnum } from './models/state.model';
+import { Actions, States } from './store';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  public settings: IAppState = null;
+  public LanguageEnum = LanguageEnum;
+
+  constructor(
+    private store: Store<any>,
+    private translateService: TranslateService
+  ) {
+    this.translateService.setDefaultLang('en');
+  }
+
+  ngOnInit() {
+
+    this.store.select(States.selectSettings).subscribe(
+      settings => {
+        this.settings = settings;
+      }
+    );
+  }
+
+  changeTemperature(value) {
+    this.settings.temperature = value;
+    this.store.dispatch(Actions.setTemperature({ payload: value }));
+  }
+
+  changeWindSpeed(value) {
+    this.settings.windSpeed = value;
+    this.store.dispatch(Actions.setWindSpeed({ payload: value }));
+  }
+
+  changeTime(value) {
+    this.settings.time = value;
+    this.store.dispatch(Actions.setTime({ payload: value }));
+  }
+
+  changeLanguage(value) {
+    this.settings.language = value;
+    this.translateService.use(this.settings.language || 'en');
+    this.store.dispatch(Actions.setLanguage({ payload: value }));
+  }
+
+  refreshLocation() {
+    this.store.dispatch(Actions.refreshLocation());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,55 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Store, StoreModule } from '@ngrx/store';
+import { Effects, Reducers } from './store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { IonicStorageModule } from '@ionic/storage';
+import { EffectsModule } from '@ngrx/effects';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicStorageModule.forRoot(),
+    StoreModule.forRoot(Reducers.AppReducers, {
+      runtimeChecks: {
+        strictStateImmutability: false,
+        strictActionImmutability: false
+      }
+    }),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25
+    }),
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  providers: [
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AndroidPermissions,
+    ScreenOrientation,
+    HttpClient
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
