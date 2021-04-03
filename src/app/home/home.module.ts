0e55ef81f2c512a6dd7282040daf00b95f3005e4
OthLah001 <oth.lahrimi1996@gmail.com>
2021-04-03 11:28:58 +0100

    Declare components in the module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,18 +2,57 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+import { HomePage } from 'src/app/home/home.page';
 
-import { HomePageRoutingModule } from './home-routing.module';
+import { HomePageRoutingModule } from 'src/app/home/home-routing.module';
+import { HomeService } from 'src/app/home/home.services';
+import { HeaderComponent } from 'src/app/home/header/header.component';
+import { CurrentComponent } from 'src/app/home/current/current.component';
+import { HourlyComponent } from 'src/app/home/hourly/hourly.component';
+import { DailyComponent } from 'src/app/home/daily/daily.component';
+import { OpenNativeSettings } from '@ionic-native/open-native-settings/ngx';
+import { Network } from '@ionic-native/network/ngx';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+import { DateTimePipeClass, TemperaturePipeClass, TimePipeClass, WindSpeedPipeClass } from './home.pipes';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+  })
   ],
-  declarations: [HomePage]
+  declarations: [
+    HomePage,
+    HeaderComponent,
+    CurrentComponent,
+    HourlyComponent,
+    DailyComponent,
+    TemperaturePipeClass,
+    WindSpeedPipeClass,
+    TimePipeClass,
+    DateTimePipeClass
+  ],
+  providers: [
+    HomeService,
+    OpenNativeSettings,
+    AndroidPermissions,
+    HttpClient
+  ]
 })
 export class HomePageModule {}
