f3b5786b4b99161e5d42b1e9ad3a17bda6881424
OthLah001 <oth.lahrimi1996@gmail.com>
2021-04-10 11:28:37 +0100

    fix: earise the old token and create new ones (security purposes)

--- src/app/home/home.services.ts
+++ src/app/home/home.services.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
+const OPEN_WEATEHR_MAP_KEY = ''; // https://home.openweathermap.org/api_keys
+const LOCATION_IQ_KEY = ''; // https://my.locationiq.com/dashboard#accesstoken
 
 @Injectable()
 export class HomeService {
@@ -10,10 +12,10 @@ export class HomeService {
   ) { }
 
   getWeather(lat, lng): Observable<any> {
-    return this.http.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&appid=de2e7cf0d58aea6494c33515039b1315`);
+    return this.http.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&appid=${OPEN_WEATEHR_MAP_KEY}`);
   }
 
   getLocalisationInfo(lat, lng): Observable<any> {
-    return this.http.get(`https://us1.locationiq.com/v1/reverse.php?key=19382964dbc382&lat=${lat}&lon=${lng}&format=json`);
+    return this.http.get(`https://us1.locationiq.com/v1/reverse.php?key=${LOCATION_IQ_KEY}&lat=${lat}&lon=${lng}&format=json`);
   }
 }
