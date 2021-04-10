b6b94993b13fb075fadb4408d019924516d9bdeb
OthLah001 <oth.lahrimi1996@gmail.com>
2021-04-10 15:22:20 +0100

    close the app when clicking on back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { State, Store } from '@ngrx/store';
 import { TranslateService } from '@ngx-translate/core';
 import { IAppState, LanguageEnum } from './models/state.model';
 import { Actions, States } from './store';
+import { MenuController, Platform } from '@ionic/angular';
+import { Plugins } from '@capacitor/core';
+const { App } = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -16,9 +19,17 @@ export class AppComponent implements OnInit {
 
   constructor(
     private store: Store<any>,
-    private translateService: TranslateService
+    private translateService: TranslateService,
+    private platform: Platform,
+    private menu: MenuController
   ) {
     this.translateService.setDefaultLang('en');
+
+    this.platform.backButton.subscribe(async data => {
+      if (!(await this.menu.isOpen())) {
+        App.exitApp();
+      }
+    });
   }
 
   ngOnInit() {
